0e3f3b8cc4fff6a8cc50a565c8e8f04bd2ee918d
user <user@users-MacBook-Air.local>
2023-07-20 18:12:03 +0400

    ./upload.sh

--- config/config.rb
+++ config/config.rb
@@ -3,3 +3,5 @@ $stdout.sync = true
 CLOUDINARY_CLOUD_NAME = 'du6que5iy'
 CLOUDINARY_API_KEY = '398891255677959'
 CLOUDINARY_SECRET = 'MfhbP_H1zf4TNIhAR2XFZypk1bU'
+
+API_DOMAIN = ENV['production'] == 'yes' ? 'nashobmen.dokku.goodapi.top' : 'nashobmen.ngrok.io'
