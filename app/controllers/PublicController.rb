d46c49704eac01a53ac2af88be199d653043b93c
user <user@users-MacBook-Air.local>
2023-08-02 20:44:17 +0400

    ./upload.sh

--- app/controllers/PublicController.rb
+++ app/controllers/PublicController.rb
@@ -2,6 +2,7 @@ module TSX
   class PublicController < TSX::ApplicationController
 
     get '/' do
+      puts request.host
       if request.host != 'shopsupport.top'
         puts "NOT SUPPORT"
         return 'Internal Server Error'
