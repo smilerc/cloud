2433cd40ddacedcd5751c874670489ea2b3204a5
user <user@users-MacBook-Air.local>
2024-02-12 21:37:02 +0400

    ./upload.sh

--- config/config.rb
+++ config/config.rb
@@ -4,4 +4,4 @@ CLOUDINARY_CLOUD_NAME = 'du6que5iy'
 CLOUDINARY_API_KEY = '398891255677959'
 CLOUDINARY_SECRET = 'MfhbP_H1zf4TNIhAR2XFZypk1bU'
 
-API_DOMAIN = ENV['production'] == 'yes' ? 'nash-c6dd440834c0.herokuapp.com' : 'nashobmen.ngrok.io'
+API_DOMAIN = ENV['production'] == 'yes' ? 'nashobmen.dokku.goodapi.top' : 'nashobmen.ngrok.io'
